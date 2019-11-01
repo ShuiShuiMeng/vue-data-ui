d1a309f905ef991641594854eb880216b9cbd8d9
ShuiShuiMeng <981904597@qq.com>
2019-11-01 14:59:25 +0800

    add vue.config.js and settings.js

---
+++ vue.config.js
@@ -0,0 +1,47 @@
+'use strict'
+const path = require('path')
+const defaultSettings = require('./src/settings.js')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+const name = defaultSettings.title || 'Vue Data Engine' //page title
+
+const port = process.env.port || process.env.npm_config.port || 9527 //port
+
+/* https://cli.vuejs.org/config */
+module.exports = {
+  publicPath: '/',
+  outputDir : 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+  },
+  configureWebpack: {
+    name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+  chainWebpack(config) {
+    // set preserveWhitespace
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.compilerOptions.preserveWhitespace = true
+        return options
+      })
+      .end()
+  }
+}
