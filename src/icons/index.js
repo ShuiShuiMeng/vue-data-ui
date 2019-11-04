f121717679e540224150852b9889a8ff1735e4ac
shuishuimeng <981904597@qq.com>
2019-11-04 17:26:40 +0800

    登录界面icon修复

--- src/icons/index.js
+++ src/icons/index.js
@@ -4,6 +4,7 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 // register globally
 Vue.component('svg-icon', SvgIcon)
 
+// 指定svg存放目录
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld/index'
 
 Vue.use(Router)
 
 export const constantRoutes = [
   {
     path: '/',
-    component: HelloWorld
+    component: () => import('@/components/HelloWorld/index'),
+    hidden: true
   },
   {
     path: '/login',
