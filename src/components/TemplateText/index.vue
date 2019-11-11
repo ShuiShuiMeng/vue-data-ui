383146e8ae966ba174fc54b21775eb32786aa94f
ShuiShuiMeng <981904597@qq.com>
2019-11-11 16:01:48 +0800

    修改template组件

--- src/components/TemplateText/index.vue
+++ src/components/TemplateText/index.vue
@@ -1,7 +1,23 @@
+<template>
+  <text-list :text-list="textList"></text-list>
+</template>
+
 <script>
 import TextList from '../TextList'
 
 export default {
-  name: 'TemplateText'
+  name: 'TemplateText',
+  components: { TextList },
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      textList: {}
+    }
+  }
 }
 </script>
